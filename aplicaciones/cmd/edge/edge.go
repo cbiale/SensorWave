64a2c3e2cec2bf06de1a46100f60f2529d2e96ff
cbiale <claudio_biale@yahoo.com.ar>
2024-10-22 18:20:06 -0300

    manejador MQTT en edge

--- aplicaciones/cmd/edge/edge.go
+++ aplicaciones/cmd/edge/edge.go
@@ -9,9 +9,11 @@ import (
 
 // app de ejemplo
 func main() {
+	// opciones a pasar al cliente MQTT
 	opciones := MQTT.NewClientOptions().AddBroker("tcp://localhost:1883")
 	opciones.SetClientID("SensorWaveMQTT")
 
+	// crea un cliente MQTT
 	cliente := MQTT.NewClient(opciones)
 	if token := cliente.Connect(); token.Wait() && token.Error() != nil {
 		panic(token.Error())
@@ -20,10 +22,11 @@ func main() {
 
 	// función que maneja los mensajes entrantes
 	manejador := func(cliente MQTT.Client, mensaje MQTT.Message) {
-		// Store the received data in a SQLite database
-		// ...
+		fmt.Printf("Mensaje recibido: %s de %s\n", mensaje.Payload(), mensaje.Topic())
 	}
 
+	// se suscribe a todos los mensajes con tópicos que comiencen con "datos/"
+	cliente.Subscribe("datos/#", 0, nil)
 	// define la función que maneja los mensajes entrantes
 	cliente.AddRoute("datos/#", manejador)
 
@@ -32,6 +35,7 @@ func main() {
 	signal.Notify(c, os.Interrupt)
 	<-c
 
+	// se desconecta del broker MQTT
 	fmt.Println("Desconectadose del broker MQTT")
 	cliente.Disconnect(1000)
 
